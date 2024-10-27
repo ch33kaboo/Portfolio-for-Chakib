9d3b9c1960700fb5c6e58b1e52cd50ef15d68df5
ch33kaboo <mc.ouzane@esi-sba.dz>
2024-10-27 07:48:02 +0100

    make layout for Skills section

--- src/lib/components/Skills.svelte
+++ src/lib/components/Skills.svelte
@@ -1,1 +1,91 @@
-<section id="skills" class="h-screen">this is Skills</section>
+<section
+	id="skills"
+	class="flex flex-col justify-center gap-5 py-28 sm:py-20 items-start mx-8 sm:mx-64"
+	style="font-family: Arial, Helvetica, sans-serif;"
+>
+	<h6 class="uppercase text-lg tracking-wide font-bold text-gray-300/90">Skills & Expertise</h6>
+	<div class="w-full flex flex-row flex-wrap justify-start items-center gap-5">
+		<!-- <p class="leading-relaxed md:leading-8 md:text-lg tracking-wide md:tracking-wider">
+			I'm a software Engineer from Algeria. A javascript enthusiast Lorem ipsum dolor sit amet
+			consectetur, adipisicing elit. Possimus architecto, itaque illo quis impedit consequuntur
+			ullam explicabo eaque tempore, repellat vel minima sint omnis cum cupiditate reprehenderit
+			voluptatibus, esse dolor.
+		</p>
+		<p class="leading-relaxed md:leading-8 md:text-lg tracking-wide md:tracking-wider">
+			I'm a software Engineer from Algeria. A javascript enthusiast Lorem ipsum dolor sit amet
+			consectetur, adipisicing elit. Possimus architecto, itaque illo quis impedit consequuntur
+			ullam explicabo eaque tempore, repellat vel minima sint omnis cum cupiditate reprehenderit
+			voluptatibus, esse dolor.
+		</p> -->
+		<div class="bg-white bg-opacity-10 min-w-48 h-56">
+			<h3>haja</h3>
+			<ul>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+			</ul>
+		</div>
+		<div class="bg-white bg-opacity-10 min-w-48 h-56">
+			<h3>haja</h3>
+			<ul>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+			</ul>
+		</div>
+		<div class="bg-white bg-opacity-10 min-w-48 h-56">
+			<h3>haja</h3>
+			<ul>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+			</ul>
+		</div>
+		<div class="bg-white bg-opacity-10 min-w-48 h-56">
+			<h3>haja</h3>
+			<ul>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+			</ul>
+		</div>
+		<div class="bg-white bg-opacity-10 min-w-48 h-56">
+			<h3>haja</h3>
+			<ul>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+			</ul>
+		</div>
+		<div class="bg-white bg-opacity-10 min-w-48 h-56">
+			<h3>haja</h3>
+			<ul>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+			</ul>
+		</div>
+		<div class="bg-white bg-opacity-10 min-w-48 h-56">
+			<h3>haja</h3>
+			<ul>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+				<li>lowla</li>
+			</ul>
+		</div>
+	</div>
+</section>
