a1de846d44727a803882bcef14d337593115048a
ch33kaboo <mc.ouzane@esi-sba.dz>
2024-10-28 23:06:02 +0100

    style skills section

--- src/lib/components/Skills.svelte
+++ src/lib/components/Skills.svelte
@@ -1,66 +1,54 @@
 <section
 	id="skills"
-	class="flex flex-col justify-center gap-5 py-28 sm:py-20 items-start mx-8 sm:mx-64"
+	class="flex flex-col justify-center gap-5 pt-28 pb-0 sm:pt-20 sm:pb-7 items-start mx-8 sm:mx-64"
 	style="font-family: Arial, Helvetica, sans-serif;"
 >
 	<h6 class="uppercase text-xl tracking-wide font-bold text-gray-300/90">Skills & Expertise</h6>
-	<div class="w-full flex flex-row flex-wrap justify-start items-center gap-5">
-		<!-- <p class="leading-relaxed md:leading-8 md:text-lg tracking-wide md:tracking-wider">
-			I'm a software Engineer from Algeria. A javascript enthusiast Lorem ipsum dolor sit amet
-			consectetur, adipisicing elit. Possimus architecto, itaque illo quis impedit consequuntur
-			ullam explicabo eaque tempore, repellat vel minima sint omnis cum cupiditate reprehenderit
-			voluptatibus, esse dolor.
-		</p>
-		<p class="leading-relaxed md:leading-8 md:text-lg tracking-wide md:tracking-wider">
-			I'm a software Engineer from Algeria. A javascript enthusiast Lorem ipsum dolor sit amet
-			consectetur, adipisicing elit. Possimus architecto, itaque illo quis impedit consequuntur
-			ullam explicabo eaque tempore, repellat vel minima sint omnis cum cupiditate reprehenderit
-			voluptatibus, esse dolor.
-		</p> -->
-		<div class="bg-white bg-opacity-10 min-w-48 h-56">
+	<div class="w-full flex flex-row flex-wrap justify-start items-start gap-y-8 gap-x-16">
+		<div class="flex flex-col gap-3 min-w-40">
 			<h6 class="font-semibold text-lg">Languages</h6>
-			<ul>
+			<ul class="list-disc list-inside">
 				<li>JavaScript</li>
 				<li>TypeScript</li>
 				<li>python</li>
 				<li>SQL</li>
 			</ul>
 		</div>
-		<div class="bg-white bg-opacity-10 min-w-48 h-56">
+		<div class="flex flex-col gap-3 min-w-40">
 			<h6 class="font-semibold text-lg">Frontend</h6>
-			<ul>
+			<ul class="list-disc list-inside">
 				<li>Svelte & SvelteKit</li>
 				<li>React</li>
 				<li>Tailwind CSS</li>
 			</ul>
 		</div>
-		<div class="bg-white bg-opacity-10 min-w-48 h-56">
+		<div class="flex flex-col gap-3 min-w-40">
 			<h6 class="font-semibold text-lg">Backend</h6>
-			<ul>
+			<ul class="list-disc list-inside">
 				<li>Node.js</li>
 				<li>Express.js</li>
 				<li>NestJS</li>
 			</ul>
 		</div>
-		<div class="bg-white bg-opacity-10 min-w-48 h-56">
+		<div class="flex flex-col gap-3 min-w-40">
 			<h6 class="font-semibold text-lg">Databases</h6>
-			<ul>
+			<ul class="list-disc list-inside">
 				<li>MongoDB</li>
 				<li>PostgreSQL</li>
 				<li>Redis</li>
 			</ul>
 		</div>
-		<div class="bg-white bg-opacity-10 min-w-48 h-56">
+		<div class="flex flex-col gap-3 min-w-40">
 			<h6 class="font-semibold text-lg">Tools & Other</h6>
-			<ul>
+			<ul class="list-disc list-inside">
 				<li>Git & GitHub</li>
 				<li>Docker</li>
 				<li>Vercel & Netlify</li>
 			</ul>
 		</div>
-		<div class="bg-white bg-opacity-10 min-w-48 h-56">
+		<div class="flex flex-col gap-3 min-w-40">
 			<h6 class="font-semibold text-lg">IT Systems</h6>
-			<ul>
+			<ul class="list-disc list-inside">
 				<li>Networking</li>
 				<li>IoT</li>
 			</ul>
