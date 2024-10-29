7b155a7617e2817cc7041211f432dc4b96d439bd
ch33kaboo <mc.ouzane@esi-sba.dz>
2024-10-29 08:28:51 +0100

    add locale to skills section

--- src/lib/components/Skills.svelte
+++ src/lib/components/Skills.svelte
@@ -1,12 +1,18 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+</script>
+
 <section
 	id="skills"
 	class="flex flex-col justify-center gap-5 pt-28 pb-0 sm:pt-20 sm:pb-7 items-start mx-8 sm:mx-64"
 	style="font-family: Arial, Helvetica, sans-serif;"
 >
-	<h6 class="uppercase text-xl tracking-wide font-bold text-gray-300/90">Skills & Expertise</h6>
+	<h6 class="uppercase text-xl tracking-wide font-bold text-gray-300/90">
+		{$_('skillsSection.title')}
+	</h6>
 	<div class="w-full flex flex-row flex-wrap justify-start items-start gap-y-8 gap-x-16">
 		<div class="flex flex-col gap-3 min-w-40">
-			<h6 class="font-semibold text-lg">Languages</h6>
+			<h6 class="font-semibold text-lg">{$_('skillsSection.languages')}</h6>
 			<ul class="list-disc list-inside">
 				<li>JavaScript</li>
 				<li>TypeScript</li>
@@ -31,7 +37,7 @@
 			</ul>
 		</div>
 		<div class="flex flex-col gap-3 min-w-40">
-			<h6 class="font-semibold text-lg">Databases</h6>
+			<h6 class="font-semibold text-lg">{$_('skillsSection.databases')}</h6>
 			<ul class="list-disc list-inside">
 				<li>MongoDB</li>
 				<li>PostgreSQL</li>
@@ -39,7 +45,7 @@
 			</ul>
 		</div>
 		<div class="flex flex-col gap-3 min-w-40">
-			<h6 class="font-semibold text-lg">Tools & Other</h6>
+			<h6 class="font-semibold text-lg">{$_('skillsSection.toolsOther')}</h6>
 			<ul class="list-disc list-inside">
 				<li>Git & GitHub</li>
 				<li>Docker</li>
@@ -47,9 +53,9 @@
 			</ul>
 		</div>
 		<div class="flex flex-col gap-3 min-w-40">
-			<h6 class="font-semibold text-lg">IT Systems</h6>
+			<h6 class="font-semibold text-lg">{$_('skillsSection.itSystems')}</h6>
 			<ul class="list-disc list-inside">
-				<li>Networking</li>
+				<li>{$_('skillsSection.networking')}</li>
 				<li>IoT</li>
 			</ul>
 		</div>
