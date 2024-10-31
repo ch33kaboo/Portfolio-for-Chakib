6929dfc77279e4b00dddba17a4d407cdb1ee4f6f
ch33kaboo <mc.ouzane@esi-sba.dz>
2024-10-31 07:04:26 +0100

    make project section

---
+++ src/lib/components/ProjectCard.svelte
@@ -0,0 +1,8 @@
+<div class="w-full flex flex-col justify-center items-start gap-2">
+	<div class="bg-gray-950 bg-opacity-40 w-full">
+		<p class="leading-relaxed md:leading-8 md:text-lg tracking-wide md:tracking-wider">hello</p>
+		<p class="leading-relaxed md:leading-8 md:text-lg tracking-wide md:tracking-wider">hello</p>
+		<p class="leading-relaxed md:leading-8 md:text-lg tracking-wide md:tracking-wider">hello</p>
+		<p class="leading-relaxed md:leading-8 md:text-lg tracking-wide md:tracking-wider">hello</p>
+	</div>
+</div>
--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -1,1 +1,17 @@
-<section id="projects" class="h-screen">this is projects</section>
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+	import ProjectCard from './ProjectCard.svelte';
+</script>
+
+<section
+	id="projects"
+	class="flex flex-col justify-center gap-5 pt-28 pb-0 sm:pt-20 sm:pb-7 items-start mx-8 sm:mx-64"
+	style="font-family: Arial, Helvetica, sans-serif;"
+>
+	<h6 class="uppercase text-xl tracking-wide font-bold text-gray-300/90">
+		{$_('projectsSection.title')}
+	</h6>
+	<ProjectCard />
+	<ProjectCard />
+	<ProjectCard />
+</section>
