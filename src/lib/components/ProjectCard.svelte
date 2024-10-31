5c55cc799b611b5d67234f85df473abf1a19bed9
ch33kaboo <mc.ouzane@esi-sba.dz>
2024-10-31 07:58:05 +0100

    make ProjectCard

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -1,8 +1,68 @@
-<div class="w-full flex flex-col justify-center items-start gap-2">
-	<div class="bg-gray-950 bg-opacity-40 w-full">
-		<p class="leading-relaxed md:leading-8 md:text-lg tracking-wide md:tracking-wider">hello</p>
-		<p class="leading-relaxed md:leading-8 md:text-lg tracking-wide md:tracking-wider">hello</p>
-		<p class="leading-relaxed md:leading-8 md:text-lg tracking-wide md:tracking-wider">hello</p>
-		<p class="leading-relaxed md:leading-8 md:text-lg tracking-wide md:tracking-wider">hello</p>
+<div class="bg-gray-50 bg-opacity-5 w-full flex flex-col gap-4">
+	<h6 class="font-semibold text-lg">Real-time Chat App</h6>
+	<div class="flex flex-col justify-start items-start gap-2">
+		<p>
+			<span class="font-semibold">Description:</span> Developed a scalable, performant and modern real-time
+			messaging app that supports rooms with admin role, message history, and user authentication.
+		</p>
+		<p>
+			<span class="font-semibold">Key Features:</span> Real-time room-based with admin role messaging,
+			Persistent chat history, Persistant unread message count for each room, Admin can add, kick, promote,
+			demote a member, Online status for users, Responsive design, and more ...
+		</p>
+		<p>
+			<span class="font-semibold">Tech Stack:</span> Golang, Fiber, Redis, Websockets, HTMX, TailwindCSS,
+			Docker.
+		</p>
+	</div>
+	<div class="flex flex-row gap-3">
+		<button
+			class="btn btn-active btn-ghost no-animation hover:border-white hover:border-opacity-45 px-6"
+		>
+			<svg
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				data-slot="icon"
+				xmlns="http://www.w3.org/2000/svg"
+				aria-hidden="true"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
+				></path>
+			</svg>
+			<p class="ml-1">Code</p>
+		</button>
+		<button
+			class="btn btn-active btn-ghost no-animation hover:border-white hover:border-opacity-45 px-6"
+		>
+			<svg
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				data-slot="icon"
+				xmlns="http://www.w3.org/2000/svg"
+				aria-hidden="true"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
+				></path>
+			</svg>
+			<p class="ml-1">Preview</p></button
+		>
 	</div>
 </div>
