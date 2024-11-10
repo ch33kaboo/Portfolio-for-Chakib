d9d634b4a8a393075468c7ea84efb46699f5e025
ch33kaboo <mc.ouzane@esi-sba.dz>
2024-11-10 10:33:48 +0100

    minor locale change

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { _ } from 'svelte-i18n';
+
 	export let projectTitle: string = '';
 	export let description: string = '';
 	export let keyFeatures: string = '';
@@ -13,12 +15,12 @@
 	<h6 class="font-semibold text-lg">{projectTitle}</h6>
 	<div class="flex flex-col justify-start items-start gap-2">
 		<p>
-			<span class="font-semibold">Description:</span>
+			<span class="font-semibold">{$_('projectsSection.card.description')}:</span>
 			{description}
 		</p>
 		{#if keyFeatures}
 			<p>
-				<span class="font-semibold">Key Features:</span>
+				<span class="font-semibold">{$_('projectsSection.card.keyFeatures')}:</span>
 				{keyFeatures}
 			</p>
 		{/if}
@@ -55,7 +57,7 @@
 								d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
 							></path>
 						</svg>
-						<p class="ml-1">Code</p>
+						<p class="ml-1">{$_('projectsSection.card.sourceCode')}</p>
 					</button>
 				</a>
 			{/if}
@@ -83,7 +85,7 @@
 								d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
 							></path>
 						</svg>
-						<p class="ml-1">Preview</p></button
+						<p class="ml-1">{$_('projectsSection.card.liveDemo')}</p></button
 					>
 				</a>
 			{/if}
