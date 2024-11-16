2ce0aad3a3db27572c6d2cfdd32c2c6ec8873a5c
ch33kaboo <mc.ouzane@esi-sba.dz>
2024-11-17 00:15:21 +0100

    minor styling changes

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -36,11 +36,10 @@
 			{#if codeURL}
 				<a href={codeURL} rel="noopener noreferrer" target="_blank" aria-label="code">
 					<button
-						class="btn btn-active btn-ghost no-animation hover:border-white hover:border-opacity-40 px-6"
+						class="btn btn-active btn-ghost flex flex-row no-animation hover:border-white hover:border-opacity-40 sm:px-6"
 					>
 						<svg
-							width="24"
-							height="24"
+							class="w-[20px] h-[20px] sm:w-[24px] sm:h-[24px]"
 							viewBox="0 0 24 24"
 							fill="none"
 							stroke="currentColor"
@@ -57,18 +56,17 @@
 								d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5"
 							></path>
 						</svg>
-						<p class="ml-1">{$_('projectsSection.card.sourceCode')}</p>
+						<p class="sm:ml-1 w-2/3 sm:w-auto">{$_('projectsSection.card.sourceCode')}</p>
 					</button>
 				</a>
 			{/if}
 			{#if previewURL}
 				<a href={previewURL} rel="noopener noreferrer" target="_blank" aria-label="preview">
 					<button
-						class="btn btn-active btn-ghost no-animation hover:border-white hover:border-opacity-40 px-6"
+						class="btn btn-active btn-ghost flex flex-row no-animation hover:border-white hover:border-opacity-40 sm:px-6"
 					>
 						<svg
-							width="24"
-							height="24"
+							class="w-[20px] h-[20px] sm:w-[24px] sm:h-[24px]"
 							viewBox="0 0 24 24"
 							fill="none"
 							stroke="currentColor"
@@ -85,7 +83,7 @@
 								d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
 							></path>
 						</svg>
-						<p class="ml-1">{$_('projectsSection.card.liveDemo')}</p></button
+						<p class="sm:ml-1 w-2/3 sm:w-auto">{$_('projectsSection.card.liveDemo')}</p></button
 					>
 				</a>
 			{/if}
