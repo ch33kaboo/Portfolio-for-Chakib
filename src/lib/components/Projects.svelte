b0cfe472e581c43798d28ee534fb9792996185dc
ch33kaboo <mc.ouzane@esi-sba.dz>
2024-12-04 13:46:31 +0100

    refactoring

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { _ } from 'svelte-i18n';
-	import ProjectCard from './ProjectCard.svelte';
+	import ProjectsList from './ProjectsList.svelte';
 </script>
 
 <section
@@ -11,15 +11,8 @@
 	<h6 class="uppercase text-xl tracking-wide font-bold text-gray-300/90">
 		{$_('projectsSection.title')}
 	</h6>
-    <p class="-mt-2 opacity-90">
+	<p class="-mt-2 text-opacity-90">
 		{$_('projectsSection.description')}
 	</p>
-	<ProjectCard
-		projectTitle={$_('projectsSection.project1.projectTitle')}
-		description={$_('projectsSection.project1.description')}
-		keyFeatures={$_('projectsSection.project1.keyFeatures')}
-		techStack={$_('projectsSection.project1.techStack')}
-		codeURL="https://example.com"
-		previewURL="https://example.com"
-	/>
+    <ProjectsList/>
 </section>
---
+++ src/lib/components/ProjectsList.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+	import ProjectCard from './ProjectCard.svelte';
+</script>
+
+<ProjectCard
+projectTitle={$_('projectsSection.project1.projectTitle')}
+description={$_('projectsSection.project1.description')}
+keyFeatures={$_('projectsSection.project1.keyFeatures')}
+techStack={$_('projectsSection.project1.techStack')}
+codeURL="https://example.com"
+previewURL="https://example.com"
+/>
