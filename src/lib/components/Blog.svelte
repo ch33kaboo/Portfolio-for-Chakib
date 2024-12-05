2f91e90660542fc298df9d4f354600b3aba41059
ch33kaboo <mc.ouzane@esi-sba.dz>
2024-12-05 22:43:21 +0100

    add Blog section

--- src/lib/components/Blog.svelte
+++ src/lib/components/Blog.svelte
@@ -1,1 +1,16 @@
-<section id="blog" class="h-screen">this is blog</section>
+<script lang="ts">
+	import { _ } from 'svelte-i18n';
+</script>
+
+<section
+	id="blog"
+	class="flex flex-col justify-center gap-5 pt-28 pb-0 sm:pt-20 sm:pb-7 items-start mx-8 sm:mx-64"
+	style="font-family: Arial, Helvetica, sans-serif;"
+>
+	<h6 class="uppercase text-xl tracking-wide font-bold text-gray-300/90">
+		{$_('blogSection.title')}
+	</h6>
+	<p class="leading-relaxed md:leading-8 md:text-lg tracking-wide md:tracking-wider">
+		{@html $_('blogSection.description')}
+	</p>
+</section>
