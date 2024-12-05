96f1b1f279a0fb3ca3428ac126face2ab31db89f
ch33kaboo <mc.ouzane@esi-sba.dz>
2024-12-05 18:50:45 +0100

    start adding projects

--- src/lib/components/ProjectsList.svelte
+++ src/lib/components/ProjectsList.svelte
@@ -4,10 +4,136 @@
 </script>
 
 <ProjectCard
-projectTitle={$_('projectsSection.project1.projectTitle')}
-description={$_('projectsSection.project1.description')}
-keyFeatures={$_('projectsSection.project1.keyFeatures')}
-techStack={$_('projectsSection.project1.techStack')}
-codeURL="https://example.com"
-previewURL="https://example.com"
+	projectTitle={$_('projectsSection.projectsContent.project1.projectTitle')}
+	description={$_('projectsSection.projectsContent.project1.description')}
+	keyFeatures={$_('projectsSection.projectsContent.project1.keyFeatures')}
+	techStack={$_('projectsSection.projectsContent.project1.techStack')}
+	codeURL="https://github.com/ch33kaboo/alphabit-website"
+	previewURL="https://alphabit.netlify.app"
+/>
+
+<ProjectCard
+	projectTitle={$_('projectsSection.projectsContent.project2.projectTitle')}
+	description={$_('projectsSection.projectsContent.project2.description')}
+	keyFeatures={$_('projectsSection.projectsContent.project2.keyFeatures')}
+	techStack={$_('projectsSection.projectsContent.project2.techStack')}
+	codeURL="https://example.com"
+	previewURL="https://example.com"
+/>
+
+<ProjectCard
+	projectTitle={$_('projectsSection.projectsContent.project3.projectTitle')}
+	description={$_('projectsSection.projectsContent.project3.description')}
+	keyFeatures={$_('projectsSection.projectsContent.project3.keyFeatures')}
+	techStack={$_('projectsSection.projectsContent.project3.techStack')}
+	codeURL="https://example.com"
+	previewURL="https://example.com"
+/>
+
+<ProjectCard
+	projectTitle={$_('projectsSection.projectsContent.project4.projectTitle')}
+	description={$_('projectsSection.projectsContent.project4.description')}
+	keyFeatures={$_('projectsSection.projectsContent.project4.keyFeatures')}
+	techStack={$_('projectsSection.projectsContent.project4.techStack')}
+	codeURL="https://example.com"
+	previewURL="https://example.com"
+/>
+
+<ProjectCard
+	projectTitle={$_('projectsSection.projectsContent.project5.projectTitle')}
+	description={$_('projectsSection.projectsContent.project5.description')}
+	keyFeatures={$_('projectsSection.projectsContent.project5.keyFeatures')}
+	techStack={$_('projectsSection.projectsContent.project5.techStack')}
+	codeURL="https://example.com"
+	previewURL="https://example.com"
+/>
+
+<ProjectCard
+	projectTitle={$_('projectsSection.projectsContent.project6.projectTitle')}
+	description={$_('projectsSection.projectsContent.project6.description')}
+	keyFeatures={$_('projectsSection.projectsContent.project6.keyFeatures')}
+	techStack={$_('projectsSection.projectsContent.project6.techStack')}
+	codeURL="https://example.com"
+	previewURL="https://example.com"
+/>
+
+<ProjectCard
+	projectTitle={$_('projectsSection.projectsContent.project7.projectTitle')}
+	description={$_('projectsSection.projectsContent.project7.description')}
+	keyFeatures={$_('projectsSection.projectsContent.project7.keyFeatures')}
+	techStack={$_('projectsSection.projectsContent.project7.techStack')}
+	codeURL="https://example.com"
+	previewURL="https://example.com"
+/>
+
+<ProjectCard
+	projectTitle={$_('projectsSection.projectsContent.project8.projectTitle')}
+	description={$_('projectsSection.projectsContent.project8.description')}
+	keyFeatures={$_('projectsSection.projectsContent.project8.keyFeatures')}
+	techStack={$_('projectsSection.projectsContent.project8.techStack')}
+	codeURL="https://example.com"
+	previewURL="https://example.com"
+/>
+
+<ProjectCard
+	projectTitle={$_('projectsSection.projectsContent.project9.projectTitle')}
+	description={$_('projectsSection.projectsContent.project9.description')}
+	keyFeatures={$_('projectsSection.projectsContent.project9.keyFeatures')}
+	techStack={$_('projectsSection.projectsContent.project9.techStack')}
+	codeURL="https://example.com"
+	previewURL="https://example.com"
+/>
+
+<ProjectCard
+	projectTitle={$_('projectsSection.projectsContent.project10.projectTitle')}
+	description={$_('projectsSection.projectsContent.project10.description')}
+	keyFeatures={$_('projectsSection.projectsContent.project10.keyFeatures')}
+	techStack={$_('projectsSection.projectsContent.project10.techStack')}
+	codeURL="https://example.com"
+	previewURL="https://example.com"
+/>
+
+<ProjectCard
+	projectTitle={$_('projectsSection.projectsContent.project11.projectTitle')}
+	description={$_('projectsSection.projectsContent.project11.description')}
+	keyFeatures={$_('projectsSection.projectsContent.project11.keyFeatures')}
+	techStack={$_('projectsSection.projectsContent.project11.techStack')}
+	codeURL="https://example.com"
+	previewURL="https://example.com"
+/>
+
+<ProjectCard
+	projectTitle={$_('projectsSection.projectsContent.project12.projectTitle')}
+	description={$_('projectsSection.projectsContent.project12.description')}
+	keyFeatures={$_('projectsSection.projectsContent.project12.keyFeatures')}
+	techStack={$_('projectsSection.projectsContent.project12.techStack')}
+	codeURL="https://example.com"
+	previewURL="https://example.com"
+/>
+
+<ProjectCard
+	projectTitle={$_('projectsSection.projectsContent.project13.projectTitle')}
+	description={$_('projectsSection.projectsContent.project13.description')}
+	keyFeatures={$_('projectsSection.projectsContent.project13.keyFeatures')}
+	techStack={$_('projectsSection.projectsContent.project13.techStack')}
+	codeURL="https://example.com"
+	previewURL="https://example.com"
+/>
+
+<ProjectCard
+	projectTitle={$_('projectsSection.projectsContent.project14.projectTitle')}
+	description={$_('projectsSection.projectsContent.project14.description')}
+	keyFeatures={$_('projectsSection.projectsContent.project14.keyFeatures')}
+	techStack={$_('projectsSection.projectsContent.project14.techStack')}
+	codeURL="https://example.com"
+	previewURL="https://example.com"
+/>
+
+<ProjectCard
+	projectTitle={$_('projectsSection.projectsContent.project15.projectTitle')}
+	description={$_('projectsSection.projectsContent.project15.description')}
+	keyFeatures={$_('projectsSection.projectsContent.project15.keyFeatures')}
+	techStack={$_('projectsSection.projectsContent.project15.techStack')}
+	codeURL="https://example.com"
+	previewURL="https://example.com"
 />
